7d328b637f7b41e90af4095e99c2c01baf362073
Maarten Kieft <maarten.acc.github@kieft.ws>
2023-05-02 21:11:39 +0200

    Implementing aggregation query

--- src/Asmp2.Server/Persistence/Repositories/StatisticRepository.cs
+++ src/Asmp2.Server/Persistence/Repositories/StatisticRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Asmp2.Server.Application.Repositories;
 using Asmp2.Server.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Asmp2.Server.Persistence.Repositories;
 
@@ -12,9 +13,8 @@ public class StatisticRepository : IStatisticRepository
         _context = context ?? throw new ArgumentNullException(nameof(context));
     }
 
-    public Task CreateStatisticsAsync()
+    public async Task CreateStatisticsAsync()
     {
-
         var startTimestamp = _context.Statistics
             .OrderByDescending(s => s.TimestampEnd)
             .FirstOrDefault()
@@ -31,13 +31,44 @@ public class StatisticRepository : IStatisticRepository
             )
         );
 
-        var statistics = _context.Measurements
-            .Where(m => m.Timestamp >= startTimestamp && m.Timestamp < endTimestamp)
-            .GroupBy(m => m.Timestamp.ToString("yyyyMMddhh"))
-            ;
-
-        var a = 1;
-
-        return Task.CompletedTask;
+        await _context.Database.ExecuteSqlRawAsync(
+            $@"INSERT INTO Statistics(
+	              MeterId
+                , TimestampStart
+                , TimestampEnd
+                , PowerUsageTotalLowStart
+                , PowerUsageTotalLowEnd
+	            , PowerUsageTotalRegularStart
+                , PowerUsageTotalRegularEnd
+                , PowerSupplyTotalLowStart
+                , PowerSupplyTotalLowEnd
+	            , PowerSupplyTotalRegularStart
+                , PowerSupplyTotalRegularEnd
+                , GasUsageTotalStart
+                , GasUsageTotalEnd
+            )
+            SELECT 
+	              MeterId
+	            , str_to_date(date_format(timestamp, '%Y-%m-%d %H:00:00'),'%Y-%m-%d %H:00:00') timestamp_start
+                , DATE_ADD(str_to_date(date_format(timestamp, '%Y-%m-%d %H:00:00'),'%Y-%m-%d %H:00:00'), INTERVAL 1 HOUR) timestamp_end
+                , min(PowerUsageTotalLow) PowerUsageTotalLowStart
+                , max(PowerUsageTotalLow) PowerUsageTotalLowEnd
+                , min(PowerUsageTotalRegular) PowerUsageTotalRegularStart
+                , max(PowerUsageTotalRegular) PowerUsageTotalRegularEnd
+	            , min(PowerSupplyTotalLow) PowerSupplyTotalLowStart
+                , max(PowerSupplyTotalLow) PowerSupplyTotalLowEnd
+                , min(PowerSupplyTotalRegular) PowerSupplyTotalRegularStart
+                , max(PowerSupplyTotalRegular) PowerSupplyTotalRegularEnd
+	            , min(GasTotal) GasTotalStart
+                , max(GasTotal) GasTotalEnd
+            FROM 
+                Measurements
+            WHERE
+	            timestamp > {startTimestamp}
+                AND timestamp < {endTimestamp}
+            GROUP BY 
+	              MeterId
+	            , str_to_date(date_format(timestamp, '%Y-%m-%d %H:00:00'),'%Y-%m-%d %H:00:00');"
+        );
     }
 }
